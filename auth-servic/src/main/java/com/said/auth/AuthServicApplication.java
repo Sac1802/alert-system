c693df43ef88836a7d2ea5e6d007e31fd09e25d9
Said <saidacostacepeda03@gmail.com>
2026-06-05 23:29:48 -0500

    feat: First commit and basic configuration

---
+++ auth-servic/src/main/java/com/said/auth/AuthServicApplication.java
@@ -0,0 +1,13 @@
+package com.said.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServicApplication.class, args);
+	}
+
+}
---
+++ auth-servic/src/test/java/com/said/auth/AuthServicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.said.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
